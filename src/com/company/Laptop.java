90822b5e4f10ff3234eccd595e8e5edbb23b91ab
surya03110612 <suryanet42@gmail.com>
2021-05-08 14:47:00 +0530

    master3

--- src/com/company/Laptop.java
+++ src/com/company/Laptop.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Laptop {
     public static void compile(){
-        System.out.println("laptop is master the code laptop for rebased");
+        System.out.println("asd is master the code laptop for rebased");
     }
 }
