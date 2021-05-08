7cd49dfb4fa3b0f978e29d7d3e9605eba22eb1a3
surya03110612 <suryanet42@gmail.com>
2021-05-08 14:34:11 +0530

    first local commit feature 1

---
+++ src/com/company/Coder.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Coder
+{
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop();
+        System.out.println("coder is coding! from feature 1");
+        laptop.compile();
+
+    }
+}
+
