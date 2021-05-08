831fcf801bece23b925c70d81f155e7a5164218c
surya03110612 <suryanet42@gmail.com>
2021-05-08 14:45:58 +0530

    rebase 2

--- src/com/company/Laptop.java
+++ src/com/company/Laptop.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Laptop {
     public static void compile(){
-        System.out.println("laptop is compiling the code laptop for rebased");
+        System.out.println("laptop is rebasing the code laptop for rebased");
     }
 }
