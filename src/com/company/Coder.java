6d1c0d5badf4337342079e4647673ec9df057b5f
surya03110612 <suryanet42@gmail.com>
2021-05-08 14:53:07 +0530

    shiva local 1

--- src/com/company/Coder.java
+++ src/com/company/Coder.java
@@ -4,7 +4,7 @@ public class Coder
 {
     public static void main(String[] args) {
         Laptop laptop = new Laptop();
-        System.out.println("coder is coding! from feature 1");
+        System.out.println("coder local is coding! from feature 1 simple");
         laptop.compile();
 
     }
